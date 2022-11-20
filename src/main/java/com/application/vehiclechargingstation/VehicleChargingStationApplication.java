04bab37766fe17fbbbd1f7849fab818dfe3a0899
mariasalmasian <maria.salmasian@gmail.com>
2022-11-20 14:12:21 +0400

    - initial commit

---
+++ src/main/java/com/application/vehiclechargingstation/VehicleChargingStationApplication.java
@@ -0,0 +1,13 @@
+package com.application.vehiclechargingstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleChargingStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleChargingStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/vehiclechargingstation/VehicleChargingStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.vehiclechargingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleChargingStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
